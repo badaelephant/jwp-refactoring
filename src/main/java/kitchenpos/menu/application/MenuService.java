6dddc2c67b2af482ab3ee42b840709de400ab962
choiyongseok90 <badaelephant>
2022-12-22 10:49:42 +0900

    refactor: 피드백 반영 - MenuValidator의 검증로직을 MenuService에 private 메서드로 변경

--- src/main/java/kitchenpos/menu/application/MenuService.java
+++ src/main/java/kitchenpos/menu/application/MenuService.java
@@ -6,29 +6,41 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import kitchenpos.common.Price;
+import kitchenpos.exception.EntityNotFoundException;
+import kitchenpos.exception.ErrorMessage;
 import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuProduct;
+import kitchenpos.menu.domain.MenuProducts;
 import kitchenpos.menu.domain.MenuRepository;
-import kitchenpos.menu.domain.MenuValidator;
 import kitchenpos.menu.dto.MenuRequest;
 import kitchenpos.menu.dto.MenuResponse;
+import kitchenpos.menugroup.domain.MenuGroup;
+import kitchenpos.menugroup.repository.MenuGroupRepository;
+import kitchenpos.product.domain.Product;
+import kitchenpos.product.repository.ProductRepository;
 
 @Service
 public class MenuService {
 	private final MenuRepository menuRepository;
-	private final MenuValidator menuValidator;
+	private final MenuGroupRepository menuGroupRepository;
+	private final ProductRepository productRepository;
 
 	public MenuService(
 		MenuRepository menuRepository,
-		MenuValidator menuValidator
+		MenuGroupRepository menuGroupRepository,
+		ProductRepository productRepository
 	) {
 		this.menuRepository = menuRepository;
-		this.menuValidator = menuValidator;
+		this.menuGroupRepository = menuGroupRepository;
+		this.productRepository = productRepository;
 	}
 
 	@Transactional
 	public MenuResponse create(final MenuRequest menuRequest) {
 		Menu menu = menuRequest.toEntity();
-		menuValidator.validate(menu);
+		validateMenuGroup(menu);
+		validatePrice(menu);
 		return MenuResponse.of(menuRepository.save(menu));
 	}
 
@@ -39,4 +51,31 @@ public class MenuService {
 			.collect(Collectors.toList());
 	}
 
+	private void validateMenuGroup(Menu menu) {
+		if (!menuGroupRepository.existsById(menu.getMenuGroupId())) {
+			throw new EntityNotFoundException(MenuGroup.ENTITY_NAME, menu.getMenuGroupId());
+		}
+	}
+
+	private void validatePrice(Menu menu) {
+		Price menuProductsPrice = getTotalPrice(menu.getMenuProducts());
+		if (menu.moreExpensive(menuProductsPrice)) {
+			throw new IllegalArgumentException(ErrorMessage.PRICE_HIGHER_THAN_MENU_PRODUCTS_TOTAL_PRICES);
+		}
+	}
+
+	private Price getTotalPrice(MenuProducts menuProducts) {
+		return menuProducts.value()
+			.stream()
+			.map(it -> productPrice(it.getProductId(), it))
+			.reduce(Price::add)
+			.orElse(Price.ZERO);
+	}
+
+	private Price productPrice(Long productId, MenuProduct menuProduct) {
+		Price price = productRepository.findById(productId)
+			.orElseThrow(() -> new EntityNotFoundException(Product.ENTITY_NAME, productId))
+			.getPrice();
+		return menuProduct.getTotalPrice(price);
+	}
 }
